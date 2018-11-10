ffd50a3ce9bc3ff7525f089206725a0a5b433d88
Noel Aby Das <noelabydas@gmail.com>
2018-11-10 13:18:42 +0530

    Added materialize frontend

--- signin.php
+++ signin.php
@@ -72,36 +72,81 @@ function test_input($data) {
 }
 
 ?>
-<html>	
+<html lang="en">
 <head>
-	<script>
-	</script>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Abel|Quicksand" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>HealthQo</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body> 
+<nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="index.html" class="brand-logo">HealthQo</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="create.php">Create Admin</a></li>
+        <li class="active"><a href="signin.php">Sign In</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="create.php">Create Admin</a></li>
+        <li class="active"><a href="signin.php">Sign In</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
 <center>
-    <h2>Sign In</h2>
-	<div class = "form_container">
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			<label>Hospital Name</label><br>
-			<input type="text" name="hosname" required><br>
-			<label>Type</label><br>
-			<select name = "type">
-			    <option value="Admin">Admin</option>	
-			    <option value="Doctor">Doctor</option>
-			    <option value="Patient">Patient</option>
-			</select><br>
-			<label>Email</label><br>
-			<input type="email" name="email" required><br>
-			<label>Password</label><br>
-			<input type="password" name="password" required><br><br>
-			<input type="submit" name="submit">	
-		</form>
-	</div>
+    <h3>Sign In</h3>
+    
+      <div class="row" style="width:80%">
+    <form class="col s12" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">home</i>
+          <input id="hosp_name" type="text" class="validate" name="hosname" required>
+          <label for="hosp_name">Hospital Name</label>
+        </div>
+        <div class="input-field col s6">
+         <select name="type">
+          <option value="" disabled selected>Who are you?</option>
+          <option value="Admin" selected="selected">Admin</option>	
+          <option value="Doctor">Doctor</option>
+          <option value="Patient">Patient</option>
+        </select>
+        <label>User Type</label>
+      </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">email</i>
+          <input id="email" name="email" type="email" class="validate">
+          <label for="email">Email</label>
+          <span class="helper-text" data-error="wrong" data-success="right" style="text-align:left">abc@xyz.com</span>
+        </div>
+        <div class="input-field col s6">
+          <i class="material-icons prefix">lock_outline</i>
+          <input id="your_pass" type="password" class="validate" name="password" required>
+          <label for="your_pass">Password</label>
+        </div>
+      </div>
+    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button>
+    </form>
+  </div>
 </center>	
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+<script>
+      $(document).ready(function(){
+    $('select').formSelect();
+  });
+    
+    </script>	
 </body>
 </html>	
